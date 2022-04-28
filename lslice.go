34a98964d74cae00ce078e9dc8e95be72161091c
OneOfOne <oneofone@gmail.com>
2022-04-28 11:31:53 -0500

    LSlice

---
+++ lslice.go
@@ -0,0 +1,140 @@
+package genh
+
+import (
+	"encoding/json"
+	"sync"
+)
+
+type LSlice[T any] struct {
+	mux sync.RWMutex
+	v   []T
+}
+
+func (s *LSlice[T]) Update(fn func(v []T) []T) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = fn(s.v)
+}
+
+func (s *LSlice[T]) Append(vs ...T) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = append(s.v, vs...)
+}
+
+func (s *LSlice[T]) Set(i int, v T) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v[i] = v
+}
+
+func (s *LSlice[T]) Insert(i int, vs ...T) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = Insert(s.v, i, vs...)
+}
+
+func (s *LSlice[T]) Delete(i, j int) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = Delete(s.v, i, j)
+}
+
+func (s *LSlice[T]) Swap(i int, v T) (old T) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	old, s.v[i] = s.v[i], v
+	return
+}
+
+func (s *LSlice[T]) SetSlice(v []T) {
+	s.mux.Lock()
+	s.v = v
+	s.mux.Unlock()
+}
+
+func (s *LSlice[T]) Sort(lessFn func(a, b T) bool) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	SortFunc(s.v, lessFn)
+}
+
+func (s *LSlice[T]) Grow(sz int) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = Grow(s.v, sz)
+}
+
+func (s *LSlice[T]) ClipTo(len_, cap_ int) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = s.v[:len_:cap_]
+}
+
+func (s *LSlice[T]) Clip() {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.v = Clip(s.v)
+}
+
+func (s *LSlice[T]) Len() int {
+	s.mux.RLock()
+	defer s.mux.RLock()
+	return len(s.v)
+}
+
+func (s *LSlice[T]) Cap() int {
+	s.mux.RLock()
+	defer s.mux.RLock()
+	return cap(s.v)
+}
+
+func (s *LSlice[T]) Get(i int) T {
+	s.mux.RLock()
+	defer s.mux.RLock()
+	return s.v[i]
+}
+
+func (s *LSlice[T]) ForEach(fn func(i int, v T) bool) {
+	s.mux.RLock()
+	defer s.mux.RLock()
+	for i, v := range s.v {
+		if !fn(i, v) {
+			return
+		}
+	}
+}
+
+func (s *LSlice[T]) Search(cmpFn func(v T) int) (v T, found bool) {
+	var i int
+	s.mux.RLock()
+	defer s.mux.RLock()
+	if i, found = BinarySearchFunc(s.v, cmpFn); found {
+		v = s.v[i]
+	}
+	return
+}
+
+func (s *LSlice[T]) Clone() []T {
+	s.mux.RLock()
+	defer s.mux.RLock()
+	return Clone(s.v)
+}
+
+func (s *LSlice[T]) LClone() *LSlice[T] {
+	s.mux.RLock()
+	defer s.mux.RLock()
+	return &LSlice[T]{v: Clone(s.v)}
+}
+
+func (lm *LSlice[T]) MarshalJSON() ([]byte, error) {
+	lm.mux.RLock()
+	defer lm.mux.RUnlock()
+	return json.Marshal(lm.v)
+}
+
+func (lm *LSlice[T]) UnmarshalJSON(p []byte) error {
+	lm.mux.Lock()
+	defer lm.mux.Unlock()
+	return json.Unmarshal(p, &lm.v)
+}
---
+++ ptr.go
@@ -0,0 +1,36 @@
+package genh
+
+import "encoding/json"
+
+func Zero[T any]() (_ T) { return }
+
+type PtrTo[T any] struct {
+	v *T
+}
+
+func (p *PtrTo[T]) Set(v T) {
+	p.v = &v
+}
+
+func (p *PtrTo[T]) Unset() {
+	p.v = nil
+}
+
+func (p *PtrTo[T]) IsSet() bool {
+	return p.v != nil
+}
+
+func (p *PtrTo[T]) Val() T {
+	if p.v != nil {
+		return *p.v
+	}
+	return Zero[T]()
+}
+
+func (p *PtrTo[T]) MarshalJSON() ([]byte, error) {
+	return json.Marshal(p.v)
+}
+
+func (p *PtrTo[T]) UnmarshalJSON(b []byte) error {
+	return json.Unmarshal(b, &p.v)
+}
