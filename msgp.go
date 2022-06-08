451317e3176ffac6ee3379cb44ba2dc215ca68ba
OneOfOne <oneofone@gmail.com>
2022-06-08 14:51:25 -0500

    clean up

--- msgp.go
+++ msgp.go
@@ -17,6 +17,11 @@ var decPool = sync.Pool{
 	},
 }
 
+func PutMsgpackDecoder(dec *msgpack.Decoder) {
+	dec.Reset(nil)
+	decPool.Put(dec)
+}
+
 var encPool = sync.Pool{
 	New: func() any {
 		enc := msgpack.NewEncoder(nil)
@@ -27,24 +32,36 @@ var encPool = sync.Pool{
 	},
 }
 
+func PutMsgpackEncoder(enc *msgpack.Encoder) {
+	enc.Reset(nil)
+	encPool.Put(enc)
+}
+
 func UnmarshalMsgpack(b []byte, v any) error {
-	dec := NewMsgpackDecoder(bytes.NewReader(b))
-	err := dec.Decode(v)
-	dec.Reset(nil)
-	decPool.Put(dec)
-	return err
+	return DecodeMsgpack(bytes.NewReader(b), v)
 }
 
 func MarshalMsgpack(v any) ([]byte, error) {
 	var buf bytes.Buffer
-	enc := NewMsgpackEncoder(&buf)
-	err := enc.Encode(v)
-	enc.Reset(nil)
-	encPool.Put(enc)
+	err := EncodeMsgpack(&buf, v)
 	return buf.Bytes(), err
 }
 
-// NewMsgpackEncoder returns a new Encoder that writes to w.
+func EncodeMsgpack(w io.Writer, v any) error {
+	enc := NewMsgpackEncoder(w)
+	err := enc.Encode(v)
+	PutMsgpackEncoder(enc)
+	return err
+}
+
+func DecodeMsgpack(r io.Reader, v any) error {
+	dec := NewMsgpackDecoder(r)
+	err := dec.Decode(v)
+	PutMsgpackDecoder(dec)
+	return err
+}
+
+// NewMsgpackDecoder returns a new Decoder that writes to w.
 // uses json CustomStrucTag, compact floats and ints.
 func NewMsgpackEncoder(w io.Writer) *msgpack.Encoder {
 	enc := encPool.Get().(*msgpack.Encoder)
